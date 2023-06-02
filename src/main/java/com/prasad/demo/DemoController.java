7a6d8629b574bd585bba025b031bf4b0a73f3dcc
Ramprasad <vrprasad@gmail.com>
2023-06-02 11:56:14 +0530

    Added hostname

--- src/main/java/com/prasad/demo/DemoController.java
+++ src/main/java/com/prasad/demo/DemoController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,9 +16,10 @@ import java.util.Date;
 public class DemoController {
 
     @RequestMapping("/hello")
-    ResponseEntity<String> sayHello() {
+    ResponseEntity<String> sayHello() throws UnknownHostException {
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        String responseText = "API healthcheck @ "+ timeStamp+" - All OK";
+        String hostName = InetAddress.getLocalHost().getHostName();
+        String responseText = "Host : "+hostName+" API healthcheck @ "+ timeStamp+" - All OK";
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body(responseText.toString());
     }
 
